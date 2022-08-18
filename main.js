2a9987ac4ab4aacda7f184b86e6290b36a84773b
grayson-solutions <jenny.andrews@grayson-solutions.com>
2022-08-18 03:15:55 +0100

    Added js file for templates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {incomeData, expenseData, daysOfWeek} from './expenseData.js'
+
 
 const fab = document.querySelector(".fab")
 const fabWrapper = document.querySelector(".fab-wrapper")
@@ -34,38 +34,7 @@ modeSelector.addEventListener("click", (e) => {
     
 })
 
-const getExpenseSummaryData = () => {
-    const template = expenseData.map((dayData, index) => {
-        return `
-            <div class="bar-container">
-                <div class="bar">
-                    <div class="bar-value" style="height: ${dayData}%;"></div>
-                </div>
-                <div class="stats-day">${daysOfWeek[index]}</div>
-            </div>
-            `
-    }).join('')  
-    summaryExpenseStats.innerHTML = ""
-    summaryExpenseStats.insertAdjacentHTML("beforeend", template)
-}
 
-const getIncomeSummaryData = () => {
-    const template = incomeData.map((dayData, index) => {
-        return `
-            <div class="bar-container">
-                <div class="bar">
-                    <div class="bar-value" style="height: ${dayData}%;"></div>
-                </div>
-                <div class="stats-day">${daysOfWeek[index]}</div>
-            </div>
-            `
-    }).join('')   
-    summaryIncomeStats.innerHTML = ""
-    summaryIncomeStats.insertAdjacentHTML("beforeend", template)
-}
-
-// getIncomeSummaryData()
-// getExpenseSummaryData()
 
 
 
